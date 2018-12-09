9e0c6d7d55a434c821519f930c26e5a8ac613474
perlancar (on pc-home) <perlancar@gmail.com>
2018-12-09 10:17:22 +0700

    Initial commit

---
+++ lib/HTTP/Tiny/Retry.pm
@@ -0,0 +1,70 @@
+package HTTP::Tiny::Retry;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+use parent 'HTTP::Tiny';
+
+sub request {
+    my $self = shift;
+
+    my $config_retries = $self->{retries} // $ENV{HTTP_TINY_RETRIES} // 3;
+    my $config_retry_delay = $self->{retry_delay} // $ENV{HTTP_TINY_RETRY_DELAY} // 2;
+
+    my $retries = 0;
+    my $res;
+    while (1) {
+        $res = $self->SUPER::request(@_);
+        return $res if $res->{status} !~ /\A[5]/;
+        last if $retries >= $config_retries;
+        $retries++;
+        log_trace "Failed requesting $url ($res->{status} - $res->{reason}), retrying" .
+            ($config_retry_delay ? " in $config_retry_delay second(s)" : "") .
+            " ($retries of $config_retries) ...";
+        sleep $self->{retry_delay};
+    }
+    $res;
+}
+
+1;
+# ABSTRACT: Retry failed HTTP::Tiny requests
+
+=head1 SYNOPSIS
+
+ use HTTP::Tiny::Retry;
+
+ my $res  = HTTP::Tiny::Retry->new(
+     # retries     => 4, # optional, default 3
+     # retry_delay => 5, # optional, default is 2
+     # ...
+ )->get("http://www.example.com/");
+
+
+=head1 DESCRIPTION
+
+This class is a subclass of L<HTTP::Tiny> that retry fail responses (a.k.a.
+responses with 5xx statuses; 4xx are considered the client's fault so we don't
+retry those).
+
+
+=head1 ENVIRONMENT
+
+=head2 HTTP_TINY_RETRIES
+
+Int. Used to set default for the L</retries> attribute.
+
+=head2 HTTP_TINY_RETRY_DELAY
+
+Int. Used to set default for the L</retry_delay> attribute.
+
+
+=head1 SEE ALSO
+
+L<HTTP::Tiny>
+
+L<HTTP::Tiny::Patch::Retry>, patch version of this module.
